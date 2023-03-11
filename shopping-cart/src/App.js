555193cf2217cb9938c4c6d61a8b848a6c35ab4c
Phlegm Mignon <dmah@ucdavis.edu>
2023-03-10 21:05:00 -0800

    feat: properly  renders items from state

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -4,25 +4,30 @@ import Shop from "./components/Shop";
 import Navbar from "./components/Navbar";
 import Cart from "./components/Cart";
 import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
-  // const [items, setItems] = useState({});
-  // const [cart, setCart] = useState({});
+  const [items, setItems] = useState({ name: "", price: "" });
+  const [cart, setCart] = useState({});
 
   return (
     <>
-      <div className="App" title="App" name="App">
+      <div className="App" data-testid="App">
         <Navbar />
         <Routes>
           <Route path="/cart" element={<Cart />} />
-
-          <Route path="/shop" element={<Shop />} />
+          <Route
+            path="/shop"
+            element={<Shop setItems={setItems} items={items} />}
+          />
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </>
   );
 
-  //Then test tab switching functionality
+  //Set items in shop.
+  //Get details of items from props
 
   //Cart containing items
   //Has checkoutbutton
--- shopping-cart/src/components/Shop.js
+++ shopping-cart/src/components/Shop.js
@@ -1,12 +1,27 @@
 import React, { useState, useEffect } from "react";
+// import uniqid from "uniqid";
 
-function Shop() {
+function Shop(props) {
+  // const [cart, setCart] = useState({});
+
+  useEffect(() => {
+    props.setItems({ name: "Banana", price: "10" });
+  }, []);
+
   return (
     <div className="Shop">
       <h3>Shop</h3>
+      <div id="itemContainer">
+        <div className="itemCard">
+          <div className="itemName">{props.items.name}</div>
+          <div className="itemPrice">$ {props.items.price}</div>
+        </div>
+      </div>
     </div>
   );
 }
+
+//handleclick for items and setstate to cart
 
 //Contains 2 items that you can add to cart
 //Do I need another file for item factory?
